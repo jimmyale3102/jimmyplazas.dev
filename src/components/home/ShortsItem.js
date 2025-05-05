3c02abe93938e52c8448c29742925b48de9fede1
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-05 15:37:42 +0700

    feat: Create ShortItem component

---
+++ src/components/home/ShortsItem.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useTheme, CardActionArea, Card, CardContent, Typography } from '@mui/material';
+import { useThemeContext, BorderRadiusCard, BackgroundColorDark, BackgroundColorLight, MarginDefault, MarginSmall } from '../../ThemeContext';
+import { YouTube } from '@mui/icons-material';
+import IconButtonComponent from '../common/IconButtonComponent';
+
+function ShortsItem({ videoId, title, videoAlt, url }) {
+  const { mode } = useThemeContext();
+  const theme = useTheme();
+  const cardBackgroundColor = theme.palette.mode === 'light' ? BackgroundColorLight : BackgroundColorDark;
+
+  return (
+    <Card
+      style={{
+        height: 'auto',
+        backgroundColor: cardBackgroundColor,
+        boxShadow: 'none',
+        borderRadius: BorderRadiusCard,
+        marginLeft: MarginSmall,
+        marginRight: MarginSmall,
+        flex: '0 0 auto'
+      }}
+    >
+      <CardActionArea target='_blank' style={{ backgroundColor: cardBackgroundColor }}>
+        <CardContent>
+          <lite-youtube
+            videoid={videoId}
+            style={{
+              width: '100%',
+              aspectRatio: '9 / 16',
+              borderRadius: 12,
+              overflow: 'hidden'
+            }}
+          />
+          <Typography
+            variant="body2"
+            fontWeight={'bold'}
+            style={{
+              marginTop: MarginSmall,
+              display: '-webkit-box',
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: 'vertical',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
+          >
+            {title}
+          </Typography>
+          <div style={{ marginTop: MarginSmall, width: "100%" }}>
+            <IconButtonComponent
+              text="Watch on YouTube"
+              href={url}
+              IconComponent={YouTube}
+            />
+          </div>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default ShortsItem;
